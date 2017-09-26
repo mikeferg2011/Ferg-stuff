bc8d780b172689e8b18893e3f73f1354c63d1183
Ferg <mikeferg2011@gmail.com>
2017-09-25 23:45:33 -0700

    update with robinhood transactions

--- Stock Performace.R
+++ Stock Performace.R
@@ -1,5 +1,15 @@
 library(data.table)
-library('zoo')
+library(zoo)
+library(ggplot2)
+
+robin <- read.csv('D://Documents/Stock Dashboard/Robinhood Transactions.csv')
+stocks <- levels(factor(robin$Ticker[robin$Ticker != 'DEPOSIT']))
+robin_buy <- robin[robin$Type == 'BUY',]
+robin_sell <- robin[robin$Type == 'SELL',]
+robin_sell$Shares <- robin_sell$Shares*-1 
+robin_sell$Total <- robin_sell$Shares*robin_sell$AmtPerShare
+robin_clean <- rbind(robin_buy,robin_sell)
+
 
 singleStock <- function(stock, startDate = as.Date('2016-01-01'), endDate = Sys.Date() - 1) {
   #dates <- seq.Date(as.Date(startDate, '%B+%d+%Y'), as.Date(endDate, '%B+%d+%Y'), by='days')
@@ -42,7 +52,36 @@ multipleStock <- function(stocks, startDate = as.Date('2016-01-01'), endDate = S
   if(fill.na){
     output <- na.locf(output)
   }
+  output$Date <- as.Date(output$Date)
+  for(stock in stocks){
+    output[,stock] <- as.numeric(output[,stock])
+  }
   return(output)
 }
 
+robin_close_price <- multipleStock(stocks, startDate = as.Date('2016-06-01'))
 
+
+library(tidyr)
+robin_tran_day <- spread(robin_clean[,c('Date', 'Ticker', 'Shares')], Ticker, Shares)
+robin_tran_day$Date <- as.Date(robin_tran_day$Date)
+all_robin <- data.frame(seq.Date(as.Date('2016-06-01'), Sys.Date()-1, by='days'))
+colnames(all_robin) <- 'Date'
+all_robin <- merge(x=all_robin, y=robin_tran_day, by='Date', all.x=TRUE)
+all_robin[is.na(all_robin)] <- 0
+
+for(stock in stocks){
+  all_robin[,stock] <- cumsum(all_robin[,stock])
+}
+
+all_robin <- merge(x=robin_close_price, y=all_robin, by='Date', suffixes = c('price', 'shares'))
+
+for(stock in stocks){
+  all_robin[,paste(stock,'AMT', sep='')] <- all_robin[,paste(stock,'price', sep='')] * all_robin[,paste(stock,'shares', sep='')]
+  all_robin[,'Total'] <- all_robin[,'Total'] + all_robin[,paste(stock,'AMT', sep='')]
+}
+all_robin[,'Total'] <- 0
+for(stock in stocks){
+  all_robin[,'Total'] <- all_robin[,'Total'] + all_robin[,paste(stock,'AMT', sep='')]
+}
+qplot(x = all_robin$Date, y = all_robin$Total, geom = 'line')
